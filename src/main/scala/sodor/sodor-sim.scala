11d9abef74168baa498ebbdc5f886e244562d94f
zicog <mrychlik@rsm.ie>
2018-04-11 08:54:40 -0700

    Test harness now at least tests the ProgramCounter component!

--- src/main/scala/sodor/sodor-sim.scala
+++ src/main/scala/sodor/sodor-sim.scala
@@ -6,36 +6,29 @@ import spinal.core.sim._
 
 import scala.util.Random
 
-
 // sodor's testbench
 object SodorSim {
   def main(args: Array[String]) {
-    SimConfig.withWave.doSim(new Sodor){dut =>
-      //Fork a process to generate the reset and the clock on the dut
+    SimConfig.withWave.doSim(new ProgramCounter){dut =>
+      // Fork a process to generate the reset and the clock on the dut
       dut.clockDomain.forkStimulus(period = 10)
 
-      var modelState = 0
       var idx = 0
       while(idx < 100){
-/*
-        //Drive the dut inputs with random values
-        dut.io.cond0 #= Random.nextBoolean()
-        dut.io.cond1 #= Random.nextBoolean()
 
-        //Wait a rising edge on the clock
+        // Drive the dut inputs
+        dut.io.pcNext #= 0
+
+        // Wait a rising edge on the clock
         dut.clockDomain.waitRisingEdge()
 
-        //Check that the dut values match with the reference model ones
-        val modelFlag = modelState == 0 || dut.io.cond1.toBoolean
-        assert(dut.io.state.toInt == modelState)
-        assert(dut.io.flag.toBoolean == modelFlag)
+        // Check that the dut values match with the reference model ones
+        assert(dut.io.pc.toInt == 0)
+        assert(dut.io.pc4.toInt == 4)
 
-        //Update the reference model value
-        if(dut.io.cond0.toBoolean) {
-          modelState = (modelState + 1) & 0xFF
-        }
+        println(dut.io.pc4.toInt, dut.io.pc4.toInt)
 
-*/        idx += 1
+        idx += 1
       }
     }
   }
