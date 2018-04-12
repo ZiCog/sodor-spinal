da6b6a17a36d4e7e00f28d52141890a45ec8b863
zicog <mrychlik@rsm.ie>
2018-04-12 10:19:47 -0700

    Test harness now setup to perform multiple tests on compiled DUT and reach into internal component signals

--- src/main/scala/sodor/sodor-sim.scala
+++ src/main/scala/sodor/sodor-sim.scala
@@ -8,6 +8,7 @@ import scala.util.Random
 
 // sodor's testbench
 object SodorSim {
+/*
   def main(args: Array[String]) {
     SimConfig.withWave.doSim(new ProgramCounter){dut =>
       // Fork a process to generate the reset and the clock on the dut
@@ -27,10 +28,70 @@ object SodorSim {
 
         // Check that the dut values match with the reference model ones
         println(modelPcNext, dut.io.pcNext.toInt, dut.io.pc.toInt, dut.io.pc4.toInt)
-        assert(dut.io.pc.toInt == modelPcNext)
-        assert(dut.io.pc4.toInt == modelPcNext + 4)
+    //    assert(dut.io.pc.toInt == modelPcNext)
+     //   assert(dut.io.pc4.toInt == modelPcNext + 4)
 
-        modelPcNext = dut.io.pc4.toInt
+        modelPcNext = modelPcNext + 4
+
+        idx += 1
+      }
+    }
+  }
+*/
+  def main(args: Array[String]) {
+    val compiled = SimConfig.withWave.compile{
+      val dut = new Sodor ;
+      dut.counter1.count.simPublic()
+      dut.programCounter.io.pc4.simPublic()
+      dut.programCounter.io.pc.simPublic()
+      dut.programCounter.io.pcNext.simPublic()
+      dut
+    }
+
+    compiled.doSim("testA") { dut =>
+
+      // Fork a process to generate the reset and the clock on the dut
+      dut.clockDomain.forkStimulus(period = 10)
+
+      var idx = 0
+      while (idx < 100) {
+
+        // Wait a rising edge on the clock
+        dut.clockDomain.waitRisingEdge()
+        dut.clockDomain.waitFallingEdge()
+
+        // Check that the dut values match with the reference model ones
+        println(dut.counter1.count.toInt)
+        //    assert(dut.io.pc.toInt == modelPcNext)
+        //   assert(dut.io.pc4.toInt == modelPcNext + 4)
+
+        idx += 1
+      }
+    }
+
+    compiled.doSim("testB") { dut =>
+
+      // Fork a process to generate the reset and the clock on the dut
+      dut.clockDomain.forkStimulus(period = 10)
+
+      var modelPcNext = 0;
+
+      var idx = 0
+      while(idx < 100){
+
+        // Drive the dut inputs
+        dut.programCounter.io.pcNext #= modelPcNext
+
+        // Wait a rising edge on the clock
+        dut.clockDomain.waitRisingEdge()
+        dut.clockDomain.waitFallingEdge()
+
+        // Check that the dut values match with the reference model ones
+        println(modelPcNext, dut.programCounter.io.pcNext.toInt, dut.programCounter.io.pc.toInt, dut.programCounter.io.pc4.toInt)
+        //    assert(dut.io.pc.toInt == modelPcNext)
+        //   assert(dut.io.pc4.toInt == modelPcNext + 4)
+
+        modelPcNext = modelPcNext + 4
 
         idx += 1
       }
