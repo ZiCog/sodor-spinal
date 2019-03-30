b9870bdc3b5bc594b8396175a47c62176b22c0f5
zicog <zicog@example.com>
2019-03-30 21:33:36 +0200

    Added class Memomory. Compatible with picorv32 bus interface

--- src/main/scala/sodor/sodor.scala
+++ src/main/scala/sodor/sodor.scala
@@ -1,7 +1,8 @@
 package Sodor
 
 import spinal.core._
-import spinal.lib._
+import spinal.lib.misc
+
 import scala.util.Random
 
 // The Sodor 1 stage RISC-V
@@ -854,10 +855,81 @@ class Sodor extends Component {
   io.dataMemory.rw := memRw
 }
 
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+
+object Tools {
+
+  def readmemh(path: String): Array[BigInt] = {
+    val buffer = new ArrayBuffer[BigInt]
+    for (line <- Source.fromFile(path).getLines) {
+      val tokens: Array[String] = line.split("(//)").map(_.trim)
+      if (tokens.length > 0 && tokens(0) != "") {
+        val i = Integer.parseInt(tokens(0), 16)
+        buffer.append(i)
+      }
+    }
+    buffer.toArray
+  }
+}
+
+class Memory (dataWidth : Int, depth : Int, initFile: String) extends Component {
+  val io = new Bundle {
+    val enable = in Bool
+    val mem_valid = in Bool
+    val mem_instr = in Bool
+    val mem_wstrb = in UInt(dataWidth / 8 bits)
+    val mem_wdata = in SInt(dataWidth bits)
+    val mem_addr = in UInt ((Math.log10(depth)/Math.log10(2.0)).toInt  bits)
+    val mem_rdata = out SInt(dataWidth bits)
+    val mem_ready = out Bool
+  }
+
+  var ddd = Math.log10(depth)/Math.log10(2.0)
+
+  val ready = Bool
+  val rdata = SInt(dataWidth bits)
+
+  def generateInitialContent = {
+    val buffer = new ArrayBuffer[BigInt]
+    for (line <- Source.fromFile(initFile).getLines) {
+      val tokens: Array[String] = line.split("(//)").map(_.trim)
+      if (tokens.length > 0 && tokens(0) != "") {
+        val i = Integer.parseInt(tokens(0), 16)
+        buffer.append(i)
+      }
+    }
+
+    for(address <- 0 until depth) yield {
+      S(buffer(address), dataWidth bits)
+    }
+  }
+
+  val memory = Mem (SInt (dataWidth bits), depth)
+  memory.init (generateInitialContent)
+
+  memory.write(
+    io.mem_addr,
+    io.mem_wdata,
+    io.enable & io.mem_valid,
+    io.mem_wstrb.asBits
+  )
+
+  // Wire-OR'ed bus outputs.
+  when (io.enable) {
+    rdata := memory(io.mem_addr)
+  } otherwise {
+    rdata := 0
+  }
+  io.mem_ready := io.mem_valid & io.enable;
+  io.mem_rdata := rdata;
+}
+
 // Generate the Sodor Verilog
 object SodorVerilog {
   def main(args: Array[String]) {
     SpinalVerilog(new Sodor).printPruned().printUnused()
+    SpinalVerilog(new Memory(32, 1024, "firmware.hex")).printPruned().printUnused()
   }
 }
 
@@ -865,5 +937,6 @@ object SodorVerilog {
 object SodorVhdl {
   def main(args: Array[String]) {
     SpinalVhdl(new Sodor).printPruned().printUnused()
+    SpinalVhdl(new Memory(32, 1024, "firmware.hex")).printPruned().printUnused()
   }
 }
